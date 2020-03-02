a86f5cb70d6b1a019b31fadefbd62c22498cdf95
GreedyMarcus <mdomahidi@gmail.com>
2020-03-02 17:46:20 +0100

    Finished project

---
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -0,0 +1,23 @@
+package hu.bme.mit.train.tachograph;
+import hu.bme.mit.train.*;
+import com.google.common.collect.Table;
+import java.time.LocalDateTime;
+
+public class TrainTachograph {
+    Table<Integer, LocalDateTime, Integer> tachographTable = HashBasedTable.create(); 
+
+    private seedTable() {
+        TrainController trainController = new TrainControllerImpl();
+        TrainUser trainUser = new TrainUserImpl();
+        
+        int joystickPos = trainUser.getJoystickPosition();
+        LocalDateTime currentTime = LocalDateTime.now();
+        int referenceSpeed = trainSystem.getReferenceSpeed();
+
+        tachographTable.put(joystickPos, currentTime, referenceSpeed); 
+    }
+
+    private Table getTable() {
+        return tachographTable;
+    }
+}
